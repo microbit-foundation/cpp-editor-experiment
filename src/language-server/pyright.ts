68002a462bf68cf646f905bfed2f8c897910dbbc
Lewis <lewishamer0@gmail.com>
2023-07-05 12:12:32 +0100

    disable pyright

--- src/language-server/pyright.ts
+++ src/language-server/pyright.ts
@@ -20,6 +20,7 @@ const workerScriptName = "pyright-main-99cf7dc02c2324815d13.worker.js";
  * These have the same lifetime as the app.
  */
 export const pyright = (language: string): LanguageServerClient | undefined => {
+  return undefined;
   // For jest.
   if (!window.Worker) {
     return undefined;
